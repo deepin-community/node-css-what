04f86a50680b8c61e977d4dd7209dd913d54549d
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 12:27:30 +0800

    feat: update node-css-what to 6.1.0

--- src/parse.spec.ts
+++ src/parse.spec.ts
@@ -2,25 +2,6 @@ import { readFileSync } from "fs";
 import { parse } from ".";
 import { tests } from "./__fixtures__/tests";
 
-describe("parse own tests", () => {
-    for (const [selector, expected, message, options] of tests) {
-        test(message, () =>
-            expect(parse(selector, options)).toStrictEqual(expected)
-        );
-    }
-});
-
-describe("Collected selectors (qwery, sizzle, nwmatcher)", () => {
-    const out = JSON.parse(
-        readFileSync(`${__dirname}/__fixtures__/out.json`, "utf8")
-    );
-    for (const s of Object.keys(out)) {
-        test(s, () => {
-            expect(parse(s)).toStrictEqual(out[s]);
-        });
-    }
-});
-
 const broken = [
     "[",
     "(",
@@ -45,19 +26,37 @@ const broken = [
     "/*",
 ];
 
-describe("Broken selectors", () => {
-    for (const selector of broken) {
-        it(`should not parse — ${selector}`, () => {
-            expect(() => parse(selector)).toThrow(Error);
-        });
-    }
+describe("Parse", () => {
+    describe("Own tests", () => {
+        for (const [selector, expected, message] of tests) {
+            test(message, () =>
+                expect(parse(selector)).toStrictEqual(expected)
+            );
+        }
+    });
+
+    describe("Collected selectors (qwery, sizzle, nwmatcher)", () => {
+        const out = JSON.parse(
+            readFileSync(`${__dirname}/__fixtures__/out.json`, "utf8")
+        );
+        for (const s of Object.keys(out)) {
+            test(s, () => {
+                expect(parse(s)).toStrictEqual(out[s]);
+            });
+        }
+    });
+
+    describe("Broken selectors", () => {
+        for (const selector of broken) {
+            it(`should not parse — ${selector}`, () => {
+                expect(() => parse(selector)).toThrow(Error);
+            });
+        }
+    });
 
     it("should ignore comments", () => {
         expect(parse("/* comment1 */ /**/ foo /*comment2*/")).toEqual([
-            [
-                { type: "descendant" },
-                { name: "foo", namespace: null, type: "tag" },
-            ],
+            [{ name: "foo", namespace: null, type: "tag" }],
         ]);
 
         expect(() => parse("/*/")).toThrowError("Comment was not terminated");
--- src/stringify.ts
+++ src/stringify.ts
@@ -1,101 +1,146 @@
-import { Selector } from "./parse";
+import { Selector, SelectorType, AttributeAction } from "./types";
 
-const actionTypes: Record<string, string> = {
-    equals: "",
-    element: "~",
-    start: "^",
-    end: "$",
-    any: "*",
-    not: "!",
-    hyphen: "|",
-};
+const attribValChars = ["\\", '"'];
+const pseudoValChars = [...attribValChars, "(", ")"];
 
-const charsToEscape = new Set([
-    ...Object.keys(actionTypes)
-        .map((typeKey) => actionTypes[typeKey])
-        .filter(Boolean),
-    ":",
-    "[",
-    "]",
-    " ",
-    "\\",
-    "(",
-    ")",
-    "'",
-]);
+const charsToEscapeInAttributeValue = new Set(
+    attribValChars.map((c) => c.charCodeAt(0))
+);
+const charsToEscapeInPseudoValue = new Set(
+    pseudoValChars.map((c) => c.charCodeAt(0))
+);
+const charsToEscapeInName = new Set(
+    [
+        ...pseudoValChars,
+        "~",
+        "^",
+        "$",
+        "*",
+        "+",
+        "!",
+        "|",
+        ":",
+        "[",
+        "]",
+        " ",
+        ".",
+    ].map((c) => c.charCodeAt(0))
+);
 
 /**
  * Turns `selector` back into a string.
  *
  * @param selector Selector to stringify.
  */
-export default function stringify(selector: Selector[][]): string {
-    return selector.map(stringifySubselector).join(", ");
+export function stringify(selector: Selector[][]): string {
+    return selector
+        .map((token) => token.map(stringifyToken).join(""))
+        .join(", ");
 }
 
-function stringifySubselector(token: Selector[]): string {
-    return token.map(stringifyToken).join("");
-}
-
-function stringifyToken(token: Selector): string {
+function stringifyToken(
+    token: Selector,
+    index: number,
+    arr: Selector[]
+): string {
     switch (token.type) {
         // Simple types
-        case "child":
-            return " > ";
-        case "parent":
-            return " < ";
-        case "sibling":
-            return " ~ ";
-        case "adjacent":
-            return " + ";
-        case "descendant":
+        case SelectorType.Child:
+            return index === 0 ? "> " : " > ";
+        case SelectorType.Parent:
+            return index === 0 ? "< " : " < ";
+        case SelectorType.Sibling:
+            return index === 0 ? "~ " : " ~ ";
+        case SelectorType.Adjacent:
+            return index === 0 ? "+ " : " + ";
+        case SelectorType.Descendant:
             return " ";
-        case "universal":
-            return `${getNamespace(token.namespace)}*`;
+        case SelectorType.ColumnCombinator:
+            return index === 0 ? "|| " : " || ";
+        case SelectorType.Universal:
+            // Return an empty string if the selector isn't needed.
+            return token.namespace === "*" &&
+                index + 1 < arr.length &&
+                "name" in arr[index + 1]
+                ? ""
+                : `${getNamespace(token.namespace)}*`;
 
-        case "tag":
+        case SelectorType.Tag:
             return getNamespacedName(token);
 
-        case "pseudo-element":
-            return `::${escapeName(token.name)}`;
+        case SelectorType.PseudoElement:
+            return `::${escapeName(token.name, charsToEscapeInName)}${
+                token.data === null
+                    ? ""
+                    : `(${escapeName(token.data, charsToEscapeInPseudoValue)})`
+            }`;
 
-        case "pseudo":
-            if (token.data === null) return `:${escapeName(token.name)}`;
-            if (typeof token.data === "string") {
-                return `:${escapeName(token.name)}(${escapeName(token.data)})`;
-            }
-            return `:${escapeName(token.name)}(${stringify(token.data)})`;
+        case SelectorType.Pseudo:
+            return `:${escapeName(token.name, charsToEscapeInName)}${
+                token.data === null
+                    ? ""
+                    : `(${
+                          typeof token.data === "string"
+                              ? escapeName(
+                                    token.data,
+                                    charsToEscapeInPseudoValue
+                                )
+                              : stringify(token.data)
+                      })`
+            }`;
 
-        case "attribute": {
+        case SelectorType.Attribute: {
             if (
                 token.name === "id" &&
-                token.action === "equals" &&
-                !token.ignoreCase &&
+                token.action === AttributeAction.Equals &&
+                token.ignoreCase === "quirks" &&
                 !token.namespace
             ) {
-                return `#${escapeName(token.value)}`;
+                return `#${escapeName(token.value, charsToEscapeInName)}`;
             }
             if (
                 token.name === "class" &&
-                token.action === "element" &&
-                !token.ignoreCase &&
+                token.action === AttributeAction.Element &&
+                token.ignoreCase === "quirks" &&
                 !token.namespace
             ) {
-                return `.${escapeName(token.value)}`;
+                return `.${escapeName(token.value, charsToEscapeInName)}`;
             }
 
             const name = getNamespacedName(token);
 
-            if (token.action === "exists") {
+            if (token.action === AttributeAction.Exists) {
                 return `[${name}]`;
             }
 
-            return `[${name}${actionTypes[token.action]}='${escapeName(
-                token.value
-            )}'${
-                token.ignoreCase ? "i" : token.ignoreCase === false ? "s" : ""
+            return `[${name}${getActionValue(token.action)}="${escapeName(
+                token.value,
+                charsToEscapeInAttributeValue
+            )}"${
+                token.ignoreCase === null ? "" : token.ignoreCase ? " i" : " s"
             }]`;
         }
+    }
+}
+
+function getActionValue(action: AttributeAction): string {
+    switch (action) {
+        case AttributeAction.Equals:
+            return "";
+        case AttributeAction.Element:
+            return "~";
+        case AttributeAction.Start:
+            return "^";
+        case AttributeAction.End:
+            return "$";
+        case AttributeAction.Any:
+            return "*";
+        case AttributeAction.Not:
+            return "!";
+        case AttributeAction.Hyphen:
+            return "|";
+        case AttributeAction.Exists:
+            throw new Error("Shouldn't be here");
     }
 }
 
@@ -103,18 +148,32 @@ function getNamespacedName(token: {
     name: string;
     namespace: string | null;
 }): string {
-    return `${getNamespace(token.namespace)}${escapeName(token.name)}`;
+    return `${getNamespace(token.namespace)}${escapeName(
+        token.name,
+        charsToEscapeInName
+    )}`;
 }
 
 function getNamespace(namespace: string | null): string {
     return namespace !== null
-        ? `${namespace === "*" ? "*" : escapeName(namespace)}|`
+        ? `${
+              namespace === "*"
+                  ? "*"
+                  : escapeName(namespace, charsToEscapeInName)
+          }|`
         : "";
 }
 
-function escapeName(str: string): string {
-    return str
-        .split("")
-        .map((c) => (charsToEscape.has(c) ? `\\${c}` : c))
-        .join("");
+function escapeName(str: string, charsToEscape: Set<number>): string {
+    let lastIdx = 0;
+    let ret = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (charsToEscape.has(str.charCodeAt(i))) {
+            ret += `${str.slice(lastIdx, i)}\\${str.charAt(i)}`;
+            lastIdx = i + 1;
+        }
+    }
+
+    return ret.length > 0 ? ret + str.slice(lastIdx) : str;
 }
